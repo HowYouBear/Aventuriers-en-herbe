2ecf01c846bd49ed80af41e517916f82eac20910
HowYouBear <72010106+HowYouBear@users.noreply.github.com>
2023-09-27 15:26:41 +0200

    contact

--- dev/react/Aventuriers en herbe/src/page/Contact/Contact.jsx
+++ dev/react/Aventuriers en herbe/src/page/Contact/Contact.jsx
@@ -1,8 +1,18 @@
+import style from "./Contact.module.scss"
+
 function Contact(){
     return(
-        <>
-            <p>Contact</p>
-        </>
+        <div className={style.frameContact}>
+            <p>Vous pouvez laissez un mail à l'adresse suivante ou alors soumettre un message directement grâce au formulaire si dessous</p>
+            <p>uneadresse@mail.com</p>
+            <form className={style.contactForm} action="">
+                <label htmlFor="object">Sujet</label>
+                <input type="text" name="object" id="object"/>
+                <label htmlFor="object">Texte</label>
+                <textarea type="text" name="object" id="object" rows="20"/>
+                <input type="submit" value="Submit"/>
+            </form>
+        </div>
     )
 }
 
