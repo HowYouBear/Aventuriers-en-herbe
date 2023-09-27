fdd71b527abd8283dc859b23c8c06b26725b9fb0
Akakira21 <kenzo@prouveze.fr>
2023-09-27 09:49:15 +0200

    Changement de taille et ajout d'un bouton retour

--- dev/react/Aventuriers en herbe/src/page/Erreur/Erreur.jsx
+++ dev/react/Aventuriers en herbe/src/page/Erreur/Erreur.jsx
@@ -5,6 +5,7 @@ function Erreur(){
         <div className={styles.erreur}>
             <div className={styles.messageErreur}>
             <p>Y'a une erreur mon gars</p>
+            <button className={styles.button} onClick="history.back()">Retour</button>
             </div>
         </div>
     )
