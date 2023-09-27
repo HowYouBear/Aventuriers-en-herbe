aec120b0da9493e516ff4aaa7b0cf0df2214c0d7
Akakira21 <kenzo@prouveze.fr>
2023-09-27 14:08:18 +0200

    Input connexion + inscription

--- dev/react/Aventuriers en herbe/src/page/Connexion/Connexion.jsx
+++ dev/react/Aventuriers en herbe/src/page/Connexion/Connexion.jsx
@@ -1,9 +1,29 @@
+import styles from './Connexion.module.scss'
+
 function Connexion(){
     return(
-        <>
-            <p>Connexion</p>
-            
-        </>
+        <div className={styles.connexionInscription}>
+        <form className={styles.connexion}>
+            <h2>Se connecter</h2>
+            <p>Addresse mail</p>
+            <input type="email" required />
+            <p>Mot de passe</p>
+            <input type="password" required />
+            <button type='submit'>Se connecter</button>
+        </form>
+        <form className={styles.inscription}>
+            <h2>Créer un compte</h2>
+            <p>Addresse mail</p>
+            <input type="email" required />
+            <p>Confirmation addresse mail</p>
+            <input type="email" required />
+            <p>Mot de passe</p>
+            <input type="password" required />
+            <p>Confirmation mot de passe</p>
+            <input type="password" required />
+            <button type='submit'>Inscription</button>
+        </form>
+        </div>
     )
 }
 
