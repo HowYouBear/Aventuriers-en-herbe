ac9613a755d44627b7b8f8f150420d9dfb5fd054
Akakira21 <kenzo@prouveze.fr>
2023-09-27 14:33:46 +0200

    Update

--- dev/react/Aventuriers en herbe/src/page/Connexion/Connexion.jsx
+++ dev/react/Aventuriers en herbe/src/page/Connexion/Connexion.jsx
@@ -1,9 +1,31 @@
+import styles from './Connexion.module.scss'
+
 function Connexion(){
     return(
-        <>
-            <p>Connexion</p>
-            
-        </>
+        <div className={styles.connexionInscription}>
+        <form className={styles.connexion}>
+            <h2>Se connecter</h2>
+            <p>Addresse mail</p>
+            <input type="email" required />
+            <p>Mot de passe</p>
+            <input type="password" required />
+            <button type='submit'>Se connecter</button>
+        </form>
+        <form className={styles.inscription}>
+            <h2>Créer un compte</h2>
+            <p>Addresse mail</p>
+            <input type="email" required />
+            <p>Confirmation addresse mail</p>
+            <input type="email" required />
+            <p>Mot de passe</p>
+            <input type="password" required />
+            <p>Confirmation mot de passe</p>
+            <input type="password" required />
+            <input type="checkbox" name="CGU" required/>
+            <label for="CGU">j'accepte les <span className={styles.sousligne}>conditions générales</span> et <br />la <span className={styles.sousligne}>politique de confidentialité</span> de <br /> "Les Aventuriers en herbe"</label>
+            <button type='submit'>Inscription</button>
+        </form>
+        </div>
     )
 }
 
