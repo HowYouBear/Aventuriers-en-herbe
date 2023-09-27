3225295d536d32a677b70031e6848010640faa28
Akakira21 <kenzo@prouveze.fr>
2023-09-27 14:19:34 +0200

    checkbox CGU et PC

--- dev/react/Aventuriers en herbe/src/page/Connexion/Connexion.jsx
+++ dev/react/Aventuriers en herbe/src/page/Connexion/Connexion.jsx
@@ -21,6 +21,8 @@ function Connexion(){
             <input type="password" required />
             <p>Confirmation mot de passe</p>
             <input type="password" required />
+            <input type="checkbox" name="CGU" required/>
+            <label for="CGU">j'accepte les conditions générales et <br />la politique de confidentialité de <br /> "Les Aventuriers en herbe"</label>
             <button type='submit'>Inscription</button>
         </form>
         </div>
