274694cda74cb70fb0092ad988d3000227246522
HowYouBear <72010106+HowYouBear@users.noreply.github.com>
2023-09-27 10:54:19 +0200

    Header update
    dropdown

--- dev/react/Aventuriers en herbe/src/component/Header/Header.jsx
+++ dev/react/Aventuriers en herbe/src/component/Header/Header.jsx
@@ -9,9 +9,28 @@ function Header(){
                 <ul>
                     <li>Accueil</li>
                     <li>Introduction</li>
+                    <div className={style.dropdown}>
+                        <li>Univers</li>
+                        <div className={style.dropdownContent}>
+                            <ul>
+                                <li>Donjon & Dragon</li>
+                                <li>Pathfinder</li>
+                                <li>Bladerunner</li>
+                                <li>Fallout</li>
+                                <li>Final Fantasy XIV</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div className={style.dropdown}>
+                        <li>Outils</li>
+                        <div className={style.dropdownContent}>
+                            <ul>
+                                <li>Lanceur de dé</li>
+                                <li>Création personnage D&D5</li>
+                            </ul>
+                        </div>
+                    </div>
                     <li>Contact</li>
-                    <li>Univers</li>
-                    <li>Outils</li>
                 </ul>
             </nav>
         </header>
