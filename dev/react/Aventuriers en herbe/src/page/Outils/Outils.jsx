48aed192c094ad2b8f4e445f0b8b85ee14761a26
Akakira21 <kenzo@prouveze.fr>
2023-09-29 11:33:43 +0200

    Link univers (vide) page outils + link

--- dev/react/Aventuriers en herbe/src/page/Outils/Outils.jsx
+++ dev/react/Aventuriers en herbe/src/page/Outils/Outils.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import styles from './Outils.module.scss'
+import {Link} from "react-router-dom"
+
 
 const Outils = () => {
+
+    function scrollUp (){
+        window.scroll(0,0)
+    }
+
     return (
-        <div>
-            <p>Page outils</p>
+        <div className={styles.frameOutils}>
+            <h1>La boite à outils des rôlistes</h1>
+            <p>Quel que soit votre niveau d'expérience dans le monde des jeux de rôle, notre Boîte à Outils est votre alliée pour une expérience de jeu exceptionnelle. Conçue pour rendre vos aventures plus captivantes, plus conviviales et surtout plus amusantes, cette boîte renferme deux outils essentiels : la création de Fiche de Personnage et le Lanceur de Dés.</p>
+            <h2>Création de personnage Donjons et Dragons</h2>
+            <p>Préparez-vous à forger votre propre destin en créant un héros unique dans l'univers riche et fantastique de Donjons et Dragons 5ème Édition. Sur cette Fiche de Personnage, personnalisez chaque détail de votre aventurier, du nom à la race, en passant par la classe, les compétences, l'équipement, l'histoire et bien plus encore. Que vous aspiriez à incarner un magicien érudit, un guerrier courageux ou un roublard rusé, cette fiche vous guidera pour créer un héros prêt à relever tous les défis de D&D. Plongez-vous dans l'univers de Donjons et Dragons, et que l'aventure commence !</p>
+            <button><Link to = "/creationpersonnage" onClick={scrollUp}>Création de personnage Donjons et Dragons</Link></button>
+            <h2>Lanceur de dès</h2>
+            <p>Les dés sont la quintessence du jeu de rôle, ajoutant une touche d'incertitude et d'excitation à chaque action. Notre Lanceur de Dés virtuel vous permet de lancer différents types de dés, du classique dé à six faces au redoutable dé à vingt faces. Vous pouvez ajuster le nombre de dés et le type de dé pour répondre aux besoins de votre aventure, que ce soit pour résoudre des combats, des énigmes, des tests de compétence ou pour tout moment crucial. Utilisez cet outil pour vivre des histoires inoubliables, des batailles épiques et des moments palpitants. Alors, plongez dans le monde des jeux de rôle et que l'aventure commence !</p>
+            <button><Link to = "/lanceurdes" onClick={scrollUp}>Lanceur de dès</Link></button>
         </div>
     );
 };
