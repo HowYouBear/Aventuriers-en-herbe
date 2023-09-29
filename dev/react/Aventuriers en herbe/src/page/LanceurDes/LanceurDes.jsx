de6357e3ee258fffbfab66455273718d256f31de
Akakira21 <kenzo@prouveze.fr>
2023-09-29 09:08:22 +0200

    Dès

--- dev/react/Aventuriers en herbe/src/page/LanceurDes/LanceurDes.jsx
+++ dev/react/Aventuriers en herbe/src/page/LanceurDes/LanceurDes.jsx
@@ -27,12 +27,12 @@ function LanceurDes() {
       <h1>Lanceur de dés</h1>
       <div className={styles.input}>
         <select className={styles.select} name="des" id="des">
-          <option value="4">d4</option>
-          <option value="6">d6</option>
-          <option value="8">d8</option>
-          <option value="10">d10</option>
-          <option value="12">d12</option>
-          <option value="20">d20</option>
+          <option value="4">D4</option>
+          <option value="6">D6</option>
+          <option value="8">D8</option>
+          <option value="10">D10</option>
+          <option value="12">D12</option>
+          <option value="20">D20</option>
         </select>
         <div className={styles.nombreDeDes}>
           <label htmlFor="quantite">Nombre de dés : </label>
