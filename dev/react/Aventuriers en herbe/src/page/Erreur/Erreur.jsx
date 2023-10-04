490a2b6fdaf686765a087880f77efb1a2092d7e8
Akakira21 <kenzo@prouveze.fr>
2023-10-04 14:46:23 +0200

    message erreur

--- dev/react/Aventuriers en herbe/src/page/Erreur/Erreur.jsx
+++ dev/react/Aventuriers en herbe/src/page/Erreur/Erreur.jsx
@@ -4,7 +4,7 @@ function Erreur(){
     return(
         <div className={styles.erreur}>
             <div className={styles.messageErreur}>
-            <p>Y'a une erreur mon gars</p>
+            <p>Une erreur est survenu</p>
             <button className={styles.button} onClick="history.back()">Retour</button>
             </div>
         </div>
