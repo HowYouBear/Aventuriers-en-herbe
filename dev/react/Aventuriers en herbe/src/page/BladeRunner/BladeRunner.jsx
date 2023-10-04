487d00120b8591e7db9d142368b295b484501dc7
Akakira21 <kenzo@prouveze.fr>
2023-10-04 11:19:23 +0200

    page bladerunner

---
+++ dev/react/Aventuriers en herbe/src/page/BladeRunner/BladeRunner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Bladerunner.module.scss'
+
+const BladeRunner = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default BladeRunner;
