5f0865d893eb093499c5206fcfa7b45d05a07bd3
Akakira21 <kenzo@prouveze.fr>
2023-10-04 11:44:16 +0200

    page Fallout vide

---
+++ dev/react/Aventuriers en herbe/src/page/Fallout/Fallout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './Fallout.module.scss'
+
+const Fallout = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Fallout;
