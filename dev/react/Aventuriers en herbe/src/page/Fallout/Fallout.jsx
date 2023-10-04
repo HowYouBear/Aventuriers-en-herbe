b206ac98d00a622a5d6cf9a9e91aa3d54a826158
Patarus <patarusgaming@gmail.com>
2023-10-04 11:46:38 +0200

    TEXTFALLOUT

--- dev/react/Aventuriers en herbe/src/page/Fallout/Fallout.jsx
+++ dev/react/Aventuriers en herbe/src/page/Fallout/Fallout.jsx
@@ -10,3 +10,27 @@ const Fallout = () => {
 };
 
 export default Fallout;
+
+// L'Apocalypse Atomique : Un Monde Né des Cendres
+
+// La Terre de Fallout n'est pas celle que nous connaissons. Suite à une guerre nucléaire dévastatrice entre les États-Unis et la Chine, le monde a été réduit à un désert irradié, parsemé de villes en ruines et de terres désolées. Le ciel est souvent couvert de nuages toxiques, et les ressources, comme l'eau propre et la nourriture, sont rares. Dans ce paysage, chaque jour est une lutte pour la survie.
+
+// Abri et Survie : Les Vestiges d'un Ancien Monde
+
+// Avant la guerre, de vastes abris souterrains, connus sous le nom d'Abri (Vault en anglais), ont été construits pour protéger une fraction de la population. Ces abris, censés être des refuges, ont souvent servi d'expériences sociologiques par la mystérieuse compagnie Vault-Tec. Des années ou des décennies après l'explosion, les résidents de ces abris ont émergé pour découvrir le nouveau monde sauvage et impitoyable.
+
+// Des Factions, des Gangs et la Lutte pour le Pouvoir
+
+// Dans le désert, diverses factions tentent de restaurer l'ordre, ou du moins leur version de l'ordre. Que ce soit la Confrérie de l'Acier, avec sa technologie avancée, la République de Nouvelle Californie, essayant d'établir un nouveau gouvernement, ou les nombreux gangs et tribus qui règnent sur des territoires, les joueurs devront choisir leurs alliés et leurs ennemis avec soin.
+
+// Mutants, Monstres et Machines : La Faune du Nouveau Monde
+
+// L'irradiation a transformé la faune et la flore, créant des créatures mutantes comme les goules, les super mutants et les créatures terrifiantes de la région. Les joueurs doivent également être prudents avec les robots, qui, malgré leur programmation obsolète, patrouillent toujours les terres, souvent perçus comme des menaces.
+
+// La Technologie et ses Séquelles : De l'Espoir à la Destruction
+
+// Le monde de Fallout est un mélange de technologie ancienne et nouvelle. Des armes à énergie, des armures assistées et des gadgets de l'ère pré-guerre côtoient des innovations post-apocalyptiques. Mais la technologie n'est pas toujours une bénédiction ; elle est souvent à l'origine des plus grands dangers du monde de Fallout.
+
+
+// cONCLUSION
+// Fallout est un univers de contrastes, où la beauté peut être trouvée dans les endroits les plus improbables, et où les choix moraux ne sont jamais simples. C'est un terrain de jeu pour des histoires de survie, de sacrifice, d'espoir et de désespoir. Dans cet environnement, les joueurs peuvent être les héros qui reconstruisent le monde, ou les opportunistes qui profitent de ses ruines.
