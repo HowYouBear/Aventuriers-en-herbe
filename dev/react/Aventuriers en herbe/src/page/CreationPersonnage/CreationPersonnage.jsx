867f352ed24dcfd81ce08437791868088e96339f
HowYouBear <72010106+HowYouBear@users.noreply.github.com>
2023-10-05 07:00:40 +0200

    correction

--- dev/react/Aventuriers en herbe/src/page/CreationPersonnage/CreationPersonnage.jsx
+++ dev/react/Aventuriers en herbe/src/page/CreationPersonnage/CreationPersonnage.jsx
@@ -172,92 +172,92 @@ function CreationPersonnage(){
     return(
         <div className={style.creationPersonnageFrame}>
             <div className={style.retourErreur}>
-                <h3>Comment construire sa fiche de personnage?</h3>
+                <h3>Comment construire sa fiche de personnage ?</h3>
                 <div className={style.section}>
-                    <h3 onClick={dropDownOne}>Partie 1: Choisir son personnage.</h3>
+                    <h3 onClick={dropDownOne}>Partie 1 : Choisir son personnage.</h3>
                     <Fade visible={isOpenOne} animateEnter>
                         <Section text={
                         <div>
-                            <p>Consulter <a href="https://online.anyflip.com/oerjk/rewd/mobile/index.html" target="blank">le livre des joueurs</a> pour découvrir les différentes races (page 17) et classes (page 45) de donjon et dragons.</p>
-                            <p>Chaque races et classes possèdent divers avantages et défaults. Le plus important, notamment pour les débutants, et de choisir une race qui nous plaît et que l'on aimerait incarner. N'oubliez pas, Donjons et Dragons et un jeu de rôle avant tout.</p>
+                            <p>Consultez <a href="https://online.anyflip.com/oerjk/rewd/mobile/index.html" target="blank">le livre des joueurs</a> pour découvrir les différentes races (page 17) et classes (page 45) de donjons et dragons.</p>
+                            <p>Chaque race et classe possèdent divers avantages et défauts. Le plus important, notamment pour les débutants, est de choisir une race qui nous plaît et que l'on aimerait incarner. N'oubliez pas, Donjons et Dragons et un jeu de rôle avant tout.</p>
                         </div>
                         }/>
                     </Fade>
                 </div>
                 <div className={style.section}>
-                    <h3 onClick={dropDownTwo}>Partie 2: Choisir ses statistiques.</h3>
+                    <h3 onClick={dropDownTwo}>Partie 2 : Choisir ses statistiques.</h3>
                     <Fade visible={isOpenTwo}>
                         <Section text={
                             <div>
                                 <p>Pour remplir la valeur d'une statistique, il suffit de remplir la case grise au-dessus de la statistique correspondante.</p>
-                                <p>Une fois votre race et classe choisis vous devez définir vos statistiques. Pour cela il existe plusieurs méthode:</p>
-                                <p className={style.underline}>La méthode classique, le lancé de dé 6:</p>
-                                <p>Lancer 4d6 pour et retirer la valeur la plus faible pour définir votre statistique. C'est à dire que le score peut être de 3 à 18.</p>
-                                <p className={style.underline}>La méthode de distribution standard:</p>
-                                <p>Distribuer les valeurs 15 14 13 10 8 parmis vos statistiques</p>
-                                <p className={style.underline}>La méthode d'achat:</p>
+                                <p>Une fois votre race et classe choisie vous devez définir vos statistiques. Pour cela, il existe plusieurs méthode :</p>
+                                <p className={style.underline}>La méthode classique, le lancer de dé 6 :</p>
+                                <p>Lancer 4d6 pour et retirer la valeur la plus faible pour définir votre statistique. C'est-à-dire que le score peut être de 3 à 18.</p>
+                                <p className={style.underline}>La méthode de distribution standard :</p>
+                                <p>Distribuer les valeurs 15 14 13 10 8 parmi vos statistiques</p>
+                                <p className={style.underline}>La méthode d'achat :</p>
                                 <p>Toutes vos statistiques commencent à 8 et disposer de 27 points à dépenser. Les scores de statistiques de 9 à 13 coûtent 1 point, alors que les scores 14 et 15 coûtent 2 points. Il n'est pas possible de dépasser un score de 15.</p>
                                 <p>Vous êtes libre de choisir la méthode de distribution des statistiques. Il est notable que la méthode des lancés et généralement la plus efficace car elle offre le plus de statistiques, si la chance vous souris.</p>
-                                <p>Une fois vos statistiques distribuées, ajoutez les bonus de votre race.</p>
-                                <p>Vous avez maintenant vos statistiques. Notez que le modifieur (valeur indiquer au dessus de la valeur inscrite) est automatiquement calculer. Pour connaitre la valeur du modifieur, il faut appliquer la formule suivante: (valeur de la statistique - 10)/2.</p>
-                                <p>La classe défini quels jet de sauvegarde vous bénéficier.</p>
-                                <p>Un total de deux compétences pour être sélectionner en plus des compétences défini par la race et classe.</p>
+                                <p>Une fois vos statistiques distribuées, ajouter les bonus de votre race.</p>
+                                <p>Vous avez maintenant vos statistiques. Notez que le modifieur (valeur indiquée au dessus de la valeur inscrite) est automatiquement calculer. Pour connaître la valeur du modifieur, il faut appliquer la formule suivante : (valeur de la statistique - 10)/2.</p>
+                                <p>La classe définie quels jets de sauvegarde vous bénéficier.</p>
+                                <p>Un total de deux compétences pour être sélectionné en plus des compétences définies par la race et classe.</p>
                             </div>
                         }/>
                     </Fade>
                 </div>
                 <div className={style.section}>
-                    <h3 onClick={dropDownThree}>Partie 3: Le reste des statistiques.</h3>
+                    <h3 onClick={dropDownThree}>Partie 3 : Le reste des statistiques.</h3>
                     <Fade visible={isOpenThree}>
                         <Section text={
                             <div>
                             <p>La classe d'armure correspond à 10 plus le modifieur de dextérité plus les éventuelles armures.</p>
                             <p>L'initiative correspond à la valeur du modifieur de dextérité.</p>
                             <p>La vitesse dépend de votre race.</p>
-                            <p>La santé est calculer en fonction de la classe. Pour savoir le dé correspondant, consulter le livre des joueurs.</p>
+                            <p>La santé est calculée en fonction de la classe. Pour savoir le dé correspondant, consulter le livre des joueurs.</p>
                             <p>Au niveau 1, la santé correspond à la valeur maximum du dé de vie plus le modificateur.</p>
-                            <p>A chaque niveau, lancez un dé de vie pour gagner autant de santé que le résultat plus votre modificateur de constitution. Vous pouvez égalemment choisir la moyenne (d6: 4, d8: 5, D10: 6, D12: 7) si votre MJ vous l'accode.</p>
-                            <p>Les points de vie temporaires correspondent à divers bonus qui peuvent être lancer en faveur du joueur (comme des sorts ou des festins). Attention, car les bonus ne sont pas cumulable.</p>
+                            <p>A chaque niveau, lancez un dé de vie pour gagner autant de santé que le résultat en ajoutant votre modificateur de constitution. Vous pouvez égalemment choisir la moyenne (d6: 4, d8: 5, D10: 6, D12: 7) si votre MJ vous l'accorde.</p>
+                            <p>Les points de vie temporaires correspondent à divers bonus qui peuvent être lancés en faveur du joueur (comme des sorts ou des festins). Attention, car les bonus ne sont pas cumulables.</p>
                             </div>
                         }/>
                     </Fade>
                 </div>
                 <div className={style.section}>
-                        <h3 onClick={dropDownFour}>Partie 4: Les jets de sauvegarde contre la mort.</h3>
+                        <h3 onClick={dropDownFour}>Partie 4 : Les jets de sauvegarde contre la mort.</h3>
                     <Fade visible={isOpenFour}>
                         <Section text={
                             <div>
-                                <p>Elle se décompose en deux partie: les succès et les echecs.</p>
-                                <p>Quand votre personnage tombe sous les 1 points de vie, il doit alors lancer chaque tour suivant un dé 20. Si le joueur fait au moins 10, c'est un succès. Une réussite critique donne trois succès alors qu'un echec critique donne 2 éches.</p>
-                                <p>Si un joueur à terre réussi à obtenir 3 succès, il se relève alors avec un point de vie après avoir reçu de l'aide. Si il s'agit de 3 échecs, référencer vous à la partie 1 de ce guide.</p>
+                                <p>Elle se décompose en deux parties: les succès et les échecs.</p>
+                                <p>Quand votre personnage tombe sous les 1 points de vie, il doit alors lancer chaque tour suivant un dé 20. Si le joueur fait au moins 10, c'est un succès. Une réussite critique donne trois succès alors qu'un échec critique donne 2 éches.</p>
+                                <p>Si un joueur à terre réussi à obtenir 3 succès, il se relève alors avec un point de vie après avoir reçu de l'aide. S'il s'agit de 3 échecs, référencer vous à la partie 1 de ce guide.</p>
                             </div>
                         }/>
                     </Fade>
                 </div>
                 <div className={style.section}>
-                    <h3 onClick={dropDownFive}>Partie 5: Le reste de la fiche.</h3>
+                    <h3 onClick={dropDownFive}>Partie 5 : Le reste de la fiche.</h3>
                     <Fade visible={isOpenFive}>
                         <Section text={
                         <div>
-                            <p>Cette partie s'appel plus communément "copier coller le contenu du livre de joueur".</p>
-                            <p>Dans les maîtrises et langue, incrivez les maitrises et langues donner par votre race et classe.</p>
-                            <p>Pour attaques et sorts, inscrivez l'équipement donner par votre classe / race</p>
-                            <p>Pour l'équipement et les pièces (cuivre, argent, électrum, or et platine), inscrivez l'équipement donner par votre classe / race</p>
-                            <p>Pour les capacités et traits, inscrivez pour une dernière fois les élèments donner par votre classe / race</p>
-                            <p>Le bonus de maîtrise est un valeur fixe. Elle commence à 2 au niveau 1 et monte de 1 point tout les 4 niveaux</p>
-                            <p>L'inspiration est une valeur offerte par votre MJ. Pour en obtenir, jouer de manière rôle play. Si votre Mj est satisfait de votre participation à l'ambiance de la partie, il peut vous offrir des points que vous pouvez utilisez en bonus pour vos jets.</p>
-                            <p>La sagesse (perception) passive corrspond à votre instinct. Sa valeur est de 10 plus votre modifieur de sagesse. Vous pouvez ajoutez votre bonus de maitrise si vous possèdez la compétence perception.</p>
+                            <p>Cette partie s'appelle plus communément "copier coller le contenu du livre de joueur".</p>
+                            <p>Dans les maîtrises et langue, incrivez les maitrises et langues données par votre race et classe.</p>
+                            <p>Pour attaques et sorts, inscrivez l'équipement donné par votre classe/race</p>
+                            <p>Pour l'équipement et les pièces (cuivre, argent, électrum, or et platine), inscrivez l'équipement donné par votre classe/race</p>
+                            <p>Pour les capacités et traits, inscrivez pour une dernière fois les élèments donnés par votre classe/race</p>
+                            <p>Le bonus de maîtrise est un valeur fixe. Elle commence à 2 au niveau 1 et monte de 1 point tous les 4 niveaux.</p>
+                            <p>L'inspiration est une valeur offerte par votre MJ. Pour en obtenir, jouer de manière rôle play. Si votre Mj est satisfait de votre participation à l'ambiance de la partie, il peut vous offrir des points que vous pouvez utiliser en bonus pour vos jets.</p>
+                            <p>La sagesse (perception) passive correspond à votre instinct. Sa valeur est de 10 plus votre modifieur de sagesse. Vous pouvez ajouter votre bonus de maîtrise si vous possédez la compétence perception.</p>
                         </div>
                         }/>
                     </Fade>
                 </div>
                 <div className={style.section}>
-                    <h3 onClick={dropDownSix}>Partie 6: Personnalité du personnage.</h3>
+                    <h3 onClick={dropDownSix}>Partie 6 : Personnalité du personnage.</h3>
                     <Fade visible={isOpenSix}>
                         <Section text={
                         <div>
-                            <p>Il est fort probable que vous ayez déjà vu l'anciennement charte d'alignement. Celle-ci à disparu en faveur de ce nouveau système offrant plus de liberté d'expression au joueur.</p>
-                            <p>Les cases sont assez intuitive en elle même: Traits de personnalité, idéaux, liens et défauts correspondent aux caractéristiques de votre personnage. Vous y êtes libre d'y d'écrire la personnalité du personnage que vous incanerez. Certains joueurs peuvent avoir du mal à remplir ces cases car ils ne savent pas encore comment ils vont intéragir avec le monde offert par le maitre de jeu. Ce n'est pas grave, car c'est case ne sont cruciale au déroulement d'une partie. Cependant, les remplirs en cours d'aventure au fur et mesure que vous contruisez l'histoire avec les différents joueurs et maitre de jeu est une bonne chose à faire (et elle vous permettra d'obtenir des points d'inspiration!).</p>
+                            <p>Il est fort probable que vous ayez déjà vu l'anciennement charte d'alignement. Celle-ci a disparu en faveur de ce nouveau système offrant plus de liberté d'expression au joueur.</p>
+                            <p>Les cases sont assez intuitives en elle même: Traits de personnalité, idéaux, liens et défauts correspondent aux caractéristiques de votre personnage. Vous y êtes libre d'y d'écrire la personnalité du personnage que vous incarnerez. Certains joueurs peuvent avoir du mal à remplir ces cases, car ils ne savent pas encore comment ils vont intéragir avec le monde offert par le maître de jeu. Ce n'est pas grave, car c'est case ne sont cruciale au déroulement d'une partie. Cependant, les remplir en cours d'aventure au fur et mesure que vous contruirez l'histoire avec les différents joueurs et maître de jeu est une bonne chose à faire (et elle vous permettra d'obtenir des points d'inspiration !).</p>
                         </div>
                         }/>
                     </Fade>
